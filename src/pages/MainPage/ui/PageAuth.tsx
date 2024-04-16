1115db2652c7316e5c879c1522017cd1cbb78bb7
Danil Yakobchuk <danilyakobchuk00@mail.ru>
2024-04-16 12:54:17 +0700

    add typing text

--- src/pages/MainPage/ui/PageAuth.tsx
+++ src/pages/MainPage/ui/PageAuth.tsx
@@ -1,44 +1,60 @@
 import styled from 'styled-components';
 import { TemplatesToolbar } from 'widgets/TemplatesToolbar';
-import { Button, Tour, TourProps } from 'antd';
-import { useRef } from 'react';
+import { Button, Flex, Tour, Typography } from 'antd';
+import { useState } from 'react';
 import { useTourState } from 'shared/states/useTourState';
+import { TypeAnimation } from 'react-type-animation';
 
 export const PageAuth = () => {
-  const ref1 = useRef(null);
-  const ref2 = useRef(null);
-  const ref3 = useRef(null);
+  const [animationStep, setAnimationStep] = useState<number>(0);
 
   const tourItems = useTourState(state => state.tourItems);
   const openTour = useTourState(state => state.openTour);
   const setOpenTour = useTourState(state => state.setOpenTour);
-  const steps: TourProps['steps'] = [
-    {
-      title: 'Upload File',
-      description: 'Put your files here.',
-      cover: (
-        <img
-          alt="tour.png"
-          src="https://user-images.githubusercontent.com/5378891/197385811-55df8480-7ff4-44bd-9d43-a7dade598d70.png"
-        />
-      ),
-      target: () => ref1.current,
-    },
-    {
-      title: 'Save',
-      description: 'Save your changes.',
-      target: () => ref2.current,
-    },
-    {
-      title: 'Other Actions',
-      description: 'Click to see other actions.',
-      target: () => ref3.current,
-    },
-  ];
   return (
     <PageWrapper>
       <Content>
-        <Button onClick={() => setOpenTour(true)}>Начать</Button>
+        <Flex vertical gap={20}>
+          <TypeAnimation
+            sequence={[
+              'Здравствуйте!',
+              100,
+              '👋 Приветствуем тебя на платформе SynthiLearn!',
+              () => {
+                setAnimationStep(1);
+              },
+            ]}
+            speed={50}
+            wrapper="span"
+            cursor={false}
+            style={{ fontSize: '2em', display: 'inline-block' }}
+          />
+          {animationStep > 0 && (
+            <TypeAnimation
+              sequence={[
+                'У нас ты найдешь не только самые актуальные курсы по различным областям знаний, но и уникальную возможность выбирать обучающие программы, соответствующие именно твоим интересам и потребностям. ',
+                () => {
+                  setAnimationStep(2);
+                },
+              ]}
+              speed={{ type: 'keyStrokeDelayInMs', value: 20 }}
+              wrapper="span"
+              cursor={true}
+              style={{
+                fontSize: '1.5em',
+                lineHeight: '2.5rem',
+                display: 'inline-block',
+              }}
+            />
+          )}
+          <Flex gap={10} align={'center'}>
+            <Typography.Title level={4}>
+              Пройдите небольшой гайд, чтобы лучше понять, как работает
+              платформа 👉
+            </Typography.Title>
+            <Button onClick={() => setOpenTour(true)}>Начать гайд</Button>
+          </Flex>
+        </Flex>
         <Tour
           open={openTour}
           onClose={() => setOpenTour(false)}
@@ -60,5 +76,6 @@ const PageWrapper = styled.div`
 `;
 
 const Content = styled.div`
+  padding: 5% 10%;
   flex-grow: 1;
 `;
